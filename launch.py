672385705729cd4475834650d44724380c0d750a
可蓝Kelan <zhangkelan2022@163.com>
2022-05-15 10:28:54 +0800

    Update launch.py

--- launch.py
+++ launch.py
@@ -10,13 +10,16 @@ app.debug = True
 
 @app.route('/', methods=["GET", "POST"])
 def home():
+    # 从json中获取以下3个参数
     signature = request.args.get("signature")
     timestamp = request.args.get("timestamp")
     nonce = request.args.get("nonce")
+    # 传参数执行函数
     return message.receive_message(signature, timestamp, nonce)
 
 
 if __name__ == '__main__':
+    # 此配置解决WSGI报错问题
     server = pywsgi.WSGIServer(("0.0.0.0", 5000), app)
     server.serve_forever()
     # app.run(host="0.0.0.0", port=5000, debug=True)
